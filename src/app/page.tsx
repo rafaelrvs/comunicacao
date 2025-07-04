59bce9a17546d33cc4a46f685683759530c9d847
rafaelrvs <rafaelvieira1333@gmail.com>
2025-07-04 11:54:23 -0300

    aplicando melhorias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import getColaboradores from "@/actions/Colaborador/getcolaborador";
-// import { verificaAuth } from "@/actions/usuario/auth";
+import { verificaAuth } from "@/actions/usuario/auth";
 import Apresentacao from "@/components/Apresentacao/Apresentacao";
 
 
 export default async function Home() {
 
   const  {colaboradores} = await getColaboradores();
-  //  const validacao  = await verificaAuth()
-   const validacao  = true
+   const validacao  = await verificaAuth()
   return (
     <main className="min-h-screen bg-gray-50">
       <Apresentacao colaboradores={colaboradores} validacao={validacao} />
